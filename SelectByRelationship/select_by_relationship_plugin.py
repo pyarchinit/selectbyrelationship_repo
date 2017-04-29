084c9472122255dc042ef751b9b10d91aeb56a7a
Salvatore Larosa <lrssvtml@gmail.com>
2017-04-29 21:42:19 +0200

    fix header file

--- SelectByRelationship/select_by_relationship_plugin.py
+++ SelectByRelationship/select_by_relationship_plugin.py
@@ -2,7 +2,7 @@
 """
 /***************************************************************************
  SelectByRelationship
-                                 A QGIS plugin
+
  The plugin allows to select records through tables based on relationships
  one-to-one or one-to-many specified inside a Qgis project.
                               -------------------
